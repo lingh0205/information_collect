6f2eb5e61713587201a899167578d487a60426ac
LinGH <lingh0205@icloud.com>
2019-07-14 15:02:05 +0800

    add LOGGER

--- src/main/java/com/lingh/information/collect/InformationCollectApplication.java
+++ src/main/java/com/lingh/information/collect/InformationCollectApplication.java
@@ -1,6 +1,8 @@
 package com.lingh.information.collect;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,8 +12,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class InformationCollectApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(InformationCollectApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(InformationCollectApplication.class, args);
+        try {
+            SpringApplication.run(InformationCollectApplication.class, args);
+        }catch (Exception e){
+            LOGGER.error("Failed to start application.", e);
+        }
     }
 
 }
